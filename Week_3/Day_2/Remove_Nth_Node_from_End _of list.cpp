206be4823ed3af01df4c3e5df6991b99c2dc0b01
Anshika2005-singh <anshikas79981@gmail.com>
2026-06-16 20:22:00 +0530

    Create Remove_Nth_Node_from_End _of list.cpp

---
+++ Week_3/Day_2/Remove_Nth_Node_from_End _of list.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        ListNode temp(0, head);   
+        ListNode* fast = &temp;
+        ListNode* slow = &temp;
+        for (int i = 0; i <= n; i++) {
+            fast = fast->next;
+        }
+        while (fast != nullptr) {
+            fast = fast->next;
+            slow = slow->next;
+        }
+        slow->next = slow->next->next;
+
+        return temp.next;
+    }
+};
