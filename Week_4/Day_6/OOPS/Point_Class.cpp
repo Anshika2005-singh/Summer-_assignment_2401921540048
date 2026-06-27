df7d5db86874d1c752c9fee8b313d6d4730052c3
Anshika2005-singh <anshikas79981@gmail.com>
2026-06-27 23:59:53 +0530

    Create Point_Class.cpp

---
+++ Week_4/Day_6/OOPS/Point_Class.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+class Point {
+private:
+    int x, y;
+
+public:
+    
+    Point() : x(0), y(0) {}
+    Point(int x, int y) : x(x), y(y) {}
+
+    
+    void setX(int x) { this->x = x; }
+    void setY(int y) { this->y = y; }
+    void setXY(int x, int y) {
+        this->x = x;
+        this->y = y;
+    }
+    
+    void display() {
+        cout << "Point(" << x << ", " << y << ")" << endl;
+    }
+};
